bfe11ae50847a5d014b5d240e366ac94beefec0c
Maksim Lanin <maksimlanin@mail.ru>
2023-02-27 08:38:39 +0300

    Семинар 1 Задание 8 ЛанинМС

--- Seminar1_Task8.py
+++ Seminar1_Task8.py
@@ -1,6 +1,6 @@
-n,m,k = int(input('Введите n: ')),int(input('Введите m: ')),int(input('Введите k: '))
-if n*m>k:
-    if k%n==0 or k%m==0:
+n, m, k = int(input('Введите n: ')), int(input('Введите m: ')), int(input('Введите k: '))
+if n * m > k:
+    if k % n == 0 or k % m == 0:
         print('Yes')
     else:
         print('No')
